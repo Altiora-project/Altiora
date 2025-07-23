d8c4d2840919baf3c9050e0d49dbadb19ef58a15
ozalexander <156056519+ozalexander@users.noreply.github.com>
2025-07-23 19:43:24 +0300

    fix: links

--- source/widgets/video-feature/data.ts
+++ source/widgets/video-feature/data.ts
@@ -5,8 +5,8 @@ const data = {
   video: 'https://www.youtube.com/embed/cbK82koKkCM',
   more: 'Больше информации ',
   links: {
-    link1: ['ЦФА РФ', '#'],
-    link2: ['Что такое токены и токенизация?', 'https://цфа.рф/'],
+    link1: ['ЦФА РФ', 'https://цфа.рф/'],
+    link2: ['Что такое токены и токенизация?', 'https://tokeon.ru/voprosy_otvety/chto_takoe_tokeny_i_tokenizaciya'],
     link3: ['Токен или ЦФА: в чем разница? ', 'https://finstore.by/publication-finstore/token-ili-cfa'],
     link4: ['Всеобщая токенизация - Коммерсантъ', 'https://www.kommersant.ru/doc/5515543'],
     link5: [
