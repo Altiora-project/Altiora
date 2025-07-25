e56a73eaf231ee5c36a2a39599f348e85d775cd6
ozalexander <156056519+ozalexander@users.noreply.github.com>
2025-07-25 13:38:04 +0300

    fix: to features

--- source/entities/trash-ui-entity/ui/video-feature/index.tsx
+++ source/entities/trash-ui-entity/ui/video-feature/index.tsx
@@ -2,7 +2,7 @@
 
 import { UIContainer } from '../ui-container'
 
-import VideoFeature from '@widgets/video-feature'
+import VideoFeature from '@features/video-feature'
 
 export const UIVideoFeature: React.FC = () => {
   return <UIContainer title="Video Feature" description="Фича отображения видео" components={<VideoFeature />} />
