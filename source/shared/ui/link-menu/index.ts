d15c4c79c52e72026edb5bf4f21797f65cad9846
Lina Kalinka <lina.kalinka@gmail.com>
2025-07-26 19:27:49 +0300

    fix

--- source/shared/ui/link-menu/index.ts
+++ source/shared/ui/link-menu/index.ts
@@ -1,2 +1,2 @@
-export { LinkMenu } from './ui/link-menu';
-export type { LinkMenuProps } from './types/types'; 
+export { LinkMenu } from './ui/link-menu'
+export type { LinkMenuProps } from './types/types'
