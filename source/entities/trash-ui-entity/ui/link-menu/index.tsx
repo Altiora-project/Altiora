6763dc39077c010327bc94f0dcd8c53776fb4517
Maksim Dahovnik <dahovnik@gmail.com>
2025-07-27 17:17:06 +0200

    merge develop

--- source/entities/trash-ui-entity/ui/link-menu/index.tsx
+++ source/entities/trash-ui-entity/ui/link-menu/index.tsx
@@ -8,11 +8,11 @@ import { LinkMenu } from '@shared/ui/link-menu'
 
 export const UILinkMenu: React.FC = () => {
   const testData = [
-    { label: "ЦФА РФ", url: "/CFA" },
-    { label: "Что такое токен и токенизация?", url: "/token" },
-    { label: "Токен или ЦФА: в чем разница?", url: "/token_or_CFA" },
-    { label: "Всеобщая токенизация - Коммеранстъ", url: "/commersant" },
-    { label: "Международный опыт токенизации активов", url: "/experience" }
+    { label: 'ЦФА РФ', url: '/CFA' },
+    { label: 'Что такое токен и токенизация?', url: '/token' },
+    { label: 'Токен или ЦФА: в чем разница?', url: '/token_or_CFA' },
+    { label: 'Всеобщая токенизация - Коммеранстъ', url: '/commersant' },
+    { label: 'Международный опыт токенизации активов', url: '/experience' }
   ]
 
   return (
@@ -36,16 +36,13 @@ export const UILinkMenu: React.FC = () => {
               display: 'flex',
               gap: '20px',
               flexWrap: 'wrap',
-              padding: '10px',
+              padding: '10px'
             }}
           >
-            <LinkMenu 
-              title="Полезные ссылки"
-              list={testData}
-            />
+            <LinkMenu title="Полезные ссылки" list={testData} />
           </div>
         </>
       }
     />
   )
-} 
+}
