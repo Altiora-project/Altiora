ad2616734b34c778d03f6012dfcde0b0b2cf2a70
Ksenia Volkova <margo.verse@yandex.ru>
2025-08-04 21:19:44 +0500

    fix: fix info block

--- source/entities/trash-ui-entity/ui/_ui-list/ui/ui-list.tsx
+++ source/entities/trash-ui-entity/ui/_ui-list/ui/ui-list.tsx
@@ -6,23 +6,22 @@ import { UIContactListItem } from '../../contact-list-item'
 import { UIFooterInfoBlock } from '../../footer-info-block'
 import { UIFormSubmitModal } from '../../form-submit-modal'
 import { UIIcons } from '../../icons'
+import { UIInfoBlock } from '../../Info-block'
 import { UIInput } from '../../input'
 import { UILinkMenu } from '../../link-menu'
 import { UILogo } from '../../logo'
 import { UIMainBlock } from '../../main-block'
 import { MainMenuMobileTest } from '../../main-menu-mobile-test'
 import { UIMarquee } from '../../marquee'
+import { OrderFormTest } from '../../order-form-test'
 import { UIProductCard } from '../../product-card'
 import { UIServiceCard } from '../../ServiceCard'
+import { UISlider } from '../../slider/slider'
 import { UISliderCard } from '../../slider-card'
 import { UIVideoFeature } from '../../video-feature'
-import { OrderFormTest } from '../../order-form-test'
-
 import classes from '../styles/styles.module.scss'
 
-
 import { UIHeroBlock } from '@entities/trash-ui-entity/ui/hero-block'
-import { UISlider } from '../../slider/slider'
 
 export const UIList: React.FC = () => {
   return (
@@ -63,6 +62,7 @@ export const UIList: React.FC = () => {
       <UILinkMenu />
       <UIProductCard />
       <OrderFormTest />
+      <UIInfoBlock />
     </div>
   )
 }
--- source/shared/ui/info-block/ui/info-block.tsx
+++ source/shared/ui/info-block/ui/info-block.tsx
@@ -2,6 +2,8 @@ import classes from '../styles/styles.module.scss'
 import { InfoBlockProps } from '../types/types'
 import clsx from 'clsx'
 
+import { MainBlock } from '@shared/ui/_main-block'
+
 export const InfoBlock: React.FC<InfoBlockProps> = ({ title, data, ...otherProps }) => {
   if (!data || data.length === 0) {
     return null
@@ -10,14 +12,12 @@ export const InfoBlock: React.FC<InfoBlockProps> = ({ title, data, ...otherProps
   const [firstCard, ...otherCards] = data
   return (
     <div className={classes.blockInfo} {...otherProps}>
-      <div className={classes.cardWrapper}>
-        {title && (
-          <div className={classes.topContainer}>
-            <h3 className={classes.title}>{title}</h3>
-            <div className={classes.space}></div>
-          </div>
-        )}
-        <div className={clsx(classes.card, classes.bigCard)}>
+      <MainBlock
+        className={clsx(classes.mainBlock, classes.cardBlock)}
+        topContent={title && <div className={classes.infoBlockTitle}>{title}</div>}
+        bottomContent={null}
+      >
+        <div className={classes.bigCard}>
           {typeof firstCard === 'string' ? (
             <p className={classes.textCard}>{firstCard}</p>
           ) : (
@@ -27,7 +27,7 @@ export const InfoBlock: React.FC<InfoBlockProps> = ({ title, data, ...otherProps
             </div>
           )}
         </div>
-      </div>
+      </MainBlock>
       <div className={classes.smallCards}>
         {otherCards.map((card, index) => (
           <div className={clsx(classes.card, classes.smallCard)} key={index}>
