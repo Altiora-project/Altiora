c3988d1f008030cefa6a3653780a52f60d46ebc6
Ksenia Volkova <margo.verse@yandex.ru>
2025-08-06 16:12:21 +0500

    feat: add service hero section

---
+++ source/features/service-hero-section/ui/service-hero-section.tsx
@@ -0,0 +1,34 @@
+import classes from '../styles/styles.module.scss'
+
+import { ServiceDelails } from '@entities/service-page/model/model'
+
+import { Slider } from '@shared/ui/slider'
+import { SliderCard } from '@shared/ui/slider-card'
+
+export const ServiceHeroSection: React.FC<{ pageData: ServiceDelails }> = ({ pageData }) => {
+  return (
+    <div>
+      <h1 className={classes.title}>{pageData.name}</h1>
+      <div className={classes.heroSection}>
+        <div className={classes.heroSection__info}>
+          <p className={classes.heroSection__description}>{pageData.info}</p>
+          {pageData.tags?.map((tag, index) => (
+            <div className={classes.heroSection__tags} key={index}>
+              <span className={classes.heroSection__tag}>{tag.name}</span>
+            </div>
+          ))}
+        </div>
+        <Slider
+          data={pageData.case_studies.map((caseStudy, index) => (
+            <SliderCard
+              key={index}
+              title={caseStudy.name}
+              description={caseStudy.info}
+              tags={caseStudy.tags?.map(tag => ({ link: `#${tag.name}`, title: tag.name }))}
+            />
+          ))}
+        />
+      </div>
+    </div>
+  )
+}
