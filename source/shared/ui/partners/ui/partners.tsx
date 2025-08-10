74e42d3717f696d7ad327528bc1d04443d930b58
ShinTerrible <shinshingipio@gmail.com>
2025-08-10 13:08:21 +0200

    data: improve code

--- source/shared/ui/partners/ui/partners.tsx
+++ source/shared/ui/partners/ui/partners.tsx
@@ -9,29 +9,57 @@ export const Partners: FC<typePartnersProps> = ({ partners }) => {
   const displayPartners = partners.length <= 5 ? [...partners, ...partners, ...partners] : partners // если объект данных меньше 5 данные дублируются для корректного отображения
   return (
     <div className={classes.container}>
-      <Marquee speed={70} loop={0} className={clsx(classes.marquee, classes.marqueeRight)} direction="right">
-        {displayPartners.map(phrase => {
-          return (
-            <div className={classes.dataContainer}>
-              <Link key={phrase.id} href={phrase.website} className={classes.linkData}>
-                {phrase.logo ? <img src={phrase.logo} className={classes.img} alt={phrase.name} /> : <>{phrase.name}</>}
-              </Link>
-            </div>
-          )
-        })}
-      </Marquee>
-
-      <Marquee speed={50} loop={0} className={clsx(classes.marquee, classes.marqueeLeft)} direction="left">
-        {displayPartners.map(phrase => {
-          return (
-            <div className={classes.dataContainer}>
-              <Link key={phrase.id} href={phrase.website} className={classes.linkData}>
-                {phrase.logo ? <img src={phrase.logo} className={classes.img} alt={phrase.name} /> : <>{phrase.name}</>}
-              </Link>
-            </div>
-          )
-        })}
-      </Marquee>
+      {[1, 2].map(item => {
+        return (
+          <Marquee
+            key={item}
+            speed={70}
+            loop={0}
+            className={clsx(classes.marquee, item % 2 === 0 ? classes.marqueeLeft : classes.marqueeRight)}
+            direction={item % 2 === 0 ? 'left' : 'right'}
+          >
+            {displayPartners.map(phrase => {
+              return (
+                <div className={classes.dataContainer}>
+                  <Link key={phrase.id} href={phrase.website} className={classes.linkData}>
+                    {phrase.logo ? (
+                      <img src={phrase.logo} className={classes.img} alt={phrase.name} />
+                    ) : (
+                      <>{phrase.name}</>
+                    )}
+                  </Link>
+                </div>
+              )
+            })}
+          </Marquee>
+        )
+      })}
     </div>
+
+    // <div className={classes.container}>
+    //   <Marquee speed={70} loop={0} className={clsx(classes.marquee, classes.marqueeRight)} direction="right">
+    //     {displayPartners.map(phrase => {
+    //       return (
+    //         <div className={classes.dataContainer}>
+    //           <Link key={phrase.id} href={phrase.website} className={classes.linkData}>
+    //             {phrase.logo ? <img src={phrase.logo} className={classes.img} alt={phrase.name} /> : <>{phrase.name}</>}
+    //           </Link>
+    //         </div>
+    //       )
+    //     })}
+    //   </Marquee>
+
+    //   <Marquee speed={50} loop={0} className={clsx(classes.marquee, classes.marqueeLeft)} direction="left">
+    //     {displayPartners.map(phrase => {
+    //       return (
+    //         <div className={classes.dataContainer}>
+    //           <Link key={phrase.id} href={phrase.website} className={classes.linkData}>
+    //             {phrase.logo ? <img src={phrase.logo} className={classes.img} alt={phrase.name} /> : <>{phrase.name}</>}
+    //           </Link>
+    //         </div>
+    //       )
+    //     })}
+    //   </Marquee>
+    // </div>
   )
 }
