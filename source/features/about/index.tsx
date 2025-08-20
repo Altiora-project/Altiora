3d31fdb540b6ed103fbf9f3c7a987ea4d09d87ed
ShinTerrible <shinshingipio@gmail.com>
2025-08-20 13:26:56 +0200

    fix: add link on company block

--- source/features/about/index.tsx
+++ source/features/about/index.tsx
@@ -4,6 +4,6 @@ import { About } from '@shared/ui/about'
 import { typeAboutDataProps } from '@shared/ui/about/types'
 import { string } from 'zod'
 
-export const GetAbout: FC<typeAboutDataProps & { id: string }> = ({ data }) => {
+export const GetAbout: FC<typeAboutDataProps> = ({ data }) => {
   return <About data={data} />
 }
