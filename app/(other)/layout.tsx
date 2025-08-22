0a1953c4e10b85c540e7968db7f3530967a657ba
Lina Kalinka-Sturis <lina.kalinka@gmail.com>
2025-08-22 19:09:30 +0300

    fix: linter (#222)

--- app/(other)/layout.tsx
+++ app/(other)/layout.tsx
@@ -1,13 +1,15 @@
 import { FC } from 'react'
+
+import '@app/styles/globals.scss'
 import type { Metadata } from 'next'
 import NextTopLoader from 'nextjs-toploader'
 
-import '@app/styles/globals.scss'
-
 import { mainMenu } from '@app/configs/main-menu.config'
 import { drukWideCyrFont, manropeFont } from '@app/fonts'
-import { RedirectAfterReload } from '@shared/ui/redirect-after-reload/RedirectAfterReload'
+
 import { Header } from '@widgets/header'
+
+import { RedirectAfterReload } from '@shared/ui/redirect-after-reload/RedirectAfterReload'
 
 export const metadata: Metadata = {
   title: 'Nextjs project',
