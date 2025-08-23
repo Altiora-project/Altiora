38720a647bc91fb83d2c6fc3d938ae799fc7f85a
Sergey Korobok <korvin@kati.ru>
2025-08-23 12:01:33 +0300

    fix:136-startup-lab-bt

--- source/shared/ui/button/types/types.ts
+++ source/shared/ui/button/types/types.ts
@@ -1,5 +1,5 @@
 export type typeButtonProps = {
-  variant?: 'primary' | 'secondary' | 'tertiary'
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'secondary-black'
   leftIcon?: React.ReactNode
   rightIcon?: React.ReactNode
   children?: React.ReactNode
