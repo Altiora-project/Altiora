2765a3a549ee51d016c6b1cce99957a639a16e5b
ShinTerrible <shinshingipio@gmail.com>
2025-08-28 15:23:42 +0200

    fix: mobile dropdown menu

--- source/shared/ui/main-menu/ui/main-menu-mobile.tsx
+++ source/shared/ui/main-menu/ui/main-menu-mobile.tsx
@@ -3,6 +3,7 @@ import type { typeMainMenuProps } from '../types/types'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import Link from 'next/link'
 import { NavIcon } from './navicon'
+import clsx from 'clsx'
 
 export const MainMenuMobile: React.FC<typeMainMenuProps> = ({ menuList }) => {
   return (
@@ -14,11 +15,17 @@ export const MainMenuMobile: React.FC<typeMainMenuProps> = ({ menuList }) => {
       <DropdownMenu.Portal>
         <DropdownMenu.Content className={classes.menuContainer} align="end" side="bottom">
           {menuList
-            .filter(item => typeof item.url === 'string')
+            .filter(item => item.url)
             .map((item, cnt) => (
-              <DropdownMenu.Item className={classes.link} key={cnt} asChild>
-                <Link href={item.url as string}>{item.label}</Link>
-              </DropdownMenu.Item>
+              item.children ?
+                item.children.map((item, index) =>
+                  <DropdownMenu.Item className={classes.link} key={index}>
+                    <Link href={item.url as string}>{item.label}</Link>
+                  </DropdownMenu.Item>
+                )
+                : <DropdownMenu.Item className={classes.link} key={cnt} asChild>
+                  <Link href={item.url as string}>{item.label}</Link>
+                </DropdownMenu.Item>
             ))}
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
