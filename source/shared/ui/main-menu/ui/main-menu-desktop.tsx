c3364c31eb8262f019a454dca8c3b37d3fccffdc
Maksim Dahovnik <dahovnik@gmail.com>
2025-08-31 21:15:04 +0200

    Fix/header layout (#269)
    * fix header layout
    
    * fix header layout

--- source/shared/ui/main-menu/ui/main-menu-desktop.tsx
+++ source/shared/ui/main-menu/ui/main-menu-desktop.tsx
@@ -1,25 +1,26 @@
+import { FC } from 'react'
+
 import classes from '../styles/main-menu-desktop.module.scss'
 import type { typeMainMenuProps } from '../types/types'
 import clsx from 'clsx'
 import Link from 'next/link'
+
 import { HeaderMenuDesktop } from '@shared/ui/header-menu-desktop'
 
-export const MainMenuDesktop: React.FC<typeMainMenuProps> = ({ menuList, className, ...otherProps }) => (
-  <div className={clsx(classes.desktopMenuContainer, className)} {...otherProps}>
-    {menuList.map(item =>
-      item.children && item.children.length ? (
-        <HeaderMenuDesktop
-          key={item.label}
-          label={item.label}
-          children={item.children
-            .filter(child => child.url)
-            .map(child => ({ url: child.url as string, label: child.label }))}
-        />
-      ) : (
-        <Link href={item.url!} key={item.label} prefetch={false}>
-          {item.label}
-        </Link>
-      )
-    )}
-  </div>
-)
+export const MainMenuDesktop: FC<typeMainMenuProps> = ({ menuList, className, ...otherProps }) => {
+  return (
+    <div className={clsx(classes.desktopMenuContainer, className)} {...otherProps}>
+      {menuList.map(item =>
+        item.children && item.children.length ? (
+          <HeaderMenuDesktop key={item.label} label={item.label}>
+            {item.children.filter(child => child.url).map(child => ({ url: child.url as string, label: child.label }))}
+          </HeaderMenuDesktop>
+        ) : (
+          <Link href={item.url!} key={item.label} prefetch={false}>
+            {item.label}
+          </Link>
+        )
+      )}
+    </div>
+  )
+}
