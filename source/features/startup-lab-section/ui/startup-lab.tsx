2cfe9beabfa9107b19477d29caf87aff941bfe11
ozalexander <156056519+ozalexander@users.noreply.github.com>
2025-09-03 02:34:50 +0300

    fix: h3 services

--- source/features/startup-lab-section/ui/startup-lab.tsx
+++ source/features/startup-lab-section/ui/startup-lab.tsx
@@ -106,7 +106,7 @@ export const StartupLab: FC<StartupLabProps> = ({
       <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen} title={title} withCloseButton={true}>
         <div
           className={classes.modalContent}
-          //style={{ padding: '20px', maxHeight: '70vh', overflow: 'auto' }}
+        //style={{ padding: '20px', maxHeight: '70vh', overflow: 'auto' }}
         >
           {isLoading ? (
             <div>Загрузка данных...</div>
