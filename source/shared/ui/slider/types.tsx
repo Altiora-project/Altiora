55960ded1428feb79a2521a438dd73d02bc9d741
Maksim Dahovnik <dahovnik@gmail.com>
2025-09-03 21:46:37 +0200

    Fix/services promo block (#275)
    * fix background-noise index
    
    * fix background-noise index
    
    * fix styles
    
    * fix services layout
    
    * fix services layout in tablet bp

--- source/shared/ui/slider/types.tsx
+++ source/shared/ui/slider/types.tsx
@@ -1,7 +1,7 @@
-import { ComponentPropsWithRef, ReactNode } from 'react'
+import { ComponentPropsWithRef, HTMLAttributes, ReactNode } from 'react'
 
 export type typeSliderProps = {
   cards: ReactNode[]
-}
+} & HTMLAttributes<HTMLDivElement>
 
 export type PropType = ComponentPropsWithRef<'button'>
